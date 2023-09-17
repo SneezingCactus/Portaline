7162cb0380469d40dbd75a97df4ddf40b51ebabd
Kristan Ruíz <itan_07@hotmail.com>
2023-09-17 12:27:56 -0700

    Formatting and cleaning my commits

--- PortalineGiveGunTrigger.cs
+++ PortalineGiveGunTrigger.cs
@@ -1,27 +1,22 @@
 ﻿using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Celeste.Mod.Portaline
 {
-    [CustomEntity("Portaline/PortalineGiveGunTrigger")]
-    internal class PortalineGiveGunTrigger : Trigger
+  [CustomEntity("Portaline/PortalineGiveGunTrigger")]
+  internal class PortalineGiveGunTrigger : Trigger
+  {
+    private readonly bool enabled;
+
+    public PortalineGiveGunTrigger(EntityData data, Vector2 offset) : base(data, offset)
     {
-        private readonly bool enabled;
-
-        public PortalineGiveGunTrigger(EntityData data, Vector2 offset) : base(data, offset)
-        {
-            enabled = data.Bool("portalgunenabled");
-        }
-
-        public override void OnEnter(Player player)
-        {
-            base.OnEnter(player);
-            PortalineModule.Session.PortalGunEnabled = enabled;
-        }
+      enabled = data.Bool("portalgunenabled");
     }
+
+    public override void OnEnter(Player player)
+    {
+      base.OnEnter(player);
+      PortalineModule.Session.PortalGunEnabled = enabled;
+    }
+  }
 }
--- PortalineModuleSession.cs
+++ PortalineModuleSession.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Celeste.Mod.Portaline
+﻿namespace Celeste.Mod.Portaline
 {
-    public class PortalineModuleSession : EverestModuleSession
-    {
-        public bool PortalGunEnabled { get; set; } = false;
-    }
+  public class PortalineModuleSession : EverestModuleSession
+  {
+    public bool PortalGunEnabled { get; set; } = false;
+  }
 }
