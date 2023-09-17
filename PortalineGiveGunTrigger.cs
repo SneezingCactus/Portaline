095608c322766e8f11b57702c7e8431c88ab2dc1
Kristan Ruíz <itan_07@hotmail.com>
2023-09-17 02:49:13 -0700

    Adding Loenn pluggin for Portaline

---
+++ PortalineGiveGunTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.Portaline
+{
+    [CustomEntity("Portaline/PortalineGiveGunTrigger")]
+    internal class PortalineGiveGunTrigger : Trigger
+    {
+        private readonly bool givenGun;
+
+        public PortalineGiveGunTrigger(EntityData data, Vector2 offset) : base(data, offset)
+        {
+            givenGun = data.Bool("givenGun");
+        }
+    }
+}
